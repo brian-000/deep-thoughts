419e0d4485cf31e6beff4466a77ae01f88ead77a
Brain <ortiz_brian323@yahoo.com>
2022-07-02 23:49:40 -0700

    added typeDefs and resolvers for Users/Thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,36 @@ const resolvers = {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
+
+    // place this inside of the `Query` nested object right after `thoughts`
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
   }
 };
-  module.exports = resolvers;
+module.exports = resolvers;
+
+/*
+Queries: Queries are how we perform GET requests and ask for data from a GraphQL API.
+
+Mutations: Mutations are how we perform POST, PUT, and DELETE requests to create or manipulate data through a GraphQL API.
+
+
+Type definitions: Type definitions, or TypeDefs for short, involves literally defining every piece of data that the client can expect to work with through a query or mutation. Every GraphQL API starts with defining this data, as this type of strict type definition will give the client more clarity as to what they are asking for and what they can expect in return. Think of this as not only defining the API endpoint, but also defining the exact data and parameters that are tied to that endpoint.
+
+Resolvers: Resolvers are simply the functions we connect to each query or mutation type definition that perform the CRUD actions that each query or mutation is expected to perform.
+*/

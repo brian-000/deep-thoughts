f4142bc083500d651e42e7880bdf888bae634d9f
Brain <ortiz_brian323@yahoo.com>
2022-07-04 01:06:29 -0700

    implemented authentication

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 
 import Home from './pages/Home';
@@ -16,9 +17,19 @@ const httpLink = createHttpLink({
   //backend server runs on 3001 and needs absolute path
   uri: '/graphql',
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 //connecting client to backend
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 function App() {
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -17,20 +18,21 @@ const Login = (props) => {
   };
 
   // submit form
- // submit form
-const handleFormSubmit = async event => {
-  event.preventDefault();
+  // submit form
+  const handleFormSubmit = async event => {
+    event.preventDefault();
 
-  try {
-    const { data } = await login({
-      variables: { ...formState }
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
 
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-  }
-};
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+  };
 
   return (
     <main className='flex-row justify-center mb-4'>
